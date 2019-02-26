1b4ed34cdf7925de072eb6b2d7be56f2e90dc2e3
ClashLuke <luke@clashproject.com>
2019-02-26 21:01:09 +0100

    Added arguments

--- documents/proof_of_concept/tests.py
+++ documents/proof_of_concept/tests.py
@@ -1,4 +1,5 @@
-from utils import squash_init, keccak_init, padr
+from utils import squash_init, keccak_init, padr, plot_data, show_help, parse_args, result_path, init
+
 
 def count(hashes):
 	values = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
@@ -20,32 +21,43 @@ def compare(hashes, iterations):
 		differences.append(curr_diff)
 	return(differences)
 	
-def evaluate_probability(hashes):
+def evaluate_probability(hashes, write = False, out = "results"):
 	counts = count(hashes)
 	total = sum(counts)
 	counts = [int(100*c/total) for c in counts]
-	for i in range(len(counts)):
-		print("Value {}has a total share of {}%".format(padr(i,3),counts[i]))
-	print('\n')
+	if write:
+		f = open(result_path("testresults.txt",out),"a")
+		for i in range(len(counts)):
+			f.write("Value {}has a total share of {}%\n".format(padr(i,3),counts[i]))
+		f.write('\n')
+		f.close()
+	else:
+		for i in range(len(counts)):
+			print("Value {}has a total share of {}%".format(padr(i,3),counts[i]))
+		print('\n')
 
-def evaluate_similarity(hashes, iterations):
+def evaluate_similarity(hashes, iterations, write = False, out = "results"):
 	differences = compare(hashes,iterations)
-	print("\t\t\t\tHash\t\t\t\t\t\t\t\t\tDifference")
-	for i in range(1,iterations+1):
-		try:
-			print(hashes[i], differences[i-1],sep='\t')
-		except:
-			break
-	print('\n')
+	if write:
+		f = open(result_path("testresults.txt",out),"a")
+		f.write("\t\t\t\tHash\t\t\t\t\t\t\t\t\tDifference\n")
+		for i in range(1,iterations+1):
+			try:
+				f.write(str(hashes[i]) + '\t' + str(differences[i-1])+'\n')
+			except:
+				break
+		f.write('\n')
+		f.close()
+	else:
+		print("\t\t\t\tHash\t\t\t\t\t\t\t\t\tDifference")
+		for i in range(1,iterations+1):
+			try:
+				print(hashes[i], differences[i-1],sep='\t')
+			except:
+				break
+		print('\n')
 
-def plot(data, name):
-	import matplotlib.pyplot as plt	
-	plt.clf()
-	plt.scatter(data[1],data[0],s=0.2)
-	plt.savefig(name+".svg")
-	plt.savefig(name+".png")
-
-def bit_histogram(hashes):
+def bit_histogram(hashes, plot = False, out = "results"):
 	values = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 	lenght = len(hashes[0])
 	for hash in hashes:
@@ -55,7 +67,7 @@ def bit_histogram(hashes):
 
 	return [values, [i for i in range(len(values))]]
 
-def bucket_histogram(hashes, buckets):
+def bucket_histogram(hashes, buckets, plot = False, out = "results"):
 	values = []
 	for i in range(buckets+1):
 		values.append(0)
@@ -63,37 +75,74 @@ def bucket_histogram(hashes, buckets):
 		values[int(hash,16)&buckets] += 1
 	return [values, [i for i in range(len(values))]]
 
-def test_collisions(hashes):
+def test_collisions(hashes, write = False, out = "results"):
 	collisions = 0
+	_hashes = hashes[:]
 	for i in range(len(hashes)):
-		if hashes[i] in hashes[0:i]+hashes[i+1:]:
+		_hash = _hashes[0]
+		_hashes = _hashes[1:]
+		if _hash in _hashes:
 			collisions = collisions + 1
-	print("Found a total number of {} Collisions.\n".format(collisions))
+	if write:
+		f = open(result_path("testresults.txt",out),"a")
+		f.write("Found a total number of {} Collisions.\n\n".format(collisions))
+		f.close()
+	else:
+		print("Found a total number of {} Collisions.\n".format(collisions))
 	return
 
 
-def test_hash_time():
-	data = squash_init()
-	print("[Squash] Total calculation took {}s".format(data[1]))
-	print("[Squash] Calculation per hash took {}ns\n".format(int(1000000000*data[2])))
-	return(data[0])
+def test_hash_time(write = False, time = False, iterations = 2**16, out = "results"):
+	if time:
+		data = squash_init(time, iterations<<4)
+		if write:
+			f = open(result_path("testresults.txt",out),"a")
+			f.write("[Squash] Calculation of {} hashes took {}s\n".format(iterations, data[0]))
+			f.write("[Squash] Calculation per hash took {}ns\n\n".format(int(1000000000*data[1])))
+			f.close()
+		else:
+			print("[Squash] Calculation of {} hashes took {}s".format(iterations, data[0]))
+			print("[Squash] Calculation per hash took {}ns\n".format(int(1000000000*data[1])))
+	return(squash_init(False, iterations))
 
-def test_keccak_time():
-	data = keccak_init()
-	print("[Keccak] Calculation of 2**29 hashes took {}s".format(data[1]))
-	print("[Keccak] Calculation per hash took {}ns\n".format(int(1000000000*data[2])))
-	return(data[0])
+def test_keccak_time(write = False, time = False, iterations = 2**16, out = "results"):
+	if time:
+		data = keccak_init(time, iterations<<4)
+		if write:
+			f = open(result_path("testresults.txt",out),"a")
+			f.write("[Keccak] Calculation of {} hashes took {}s\n".format(iterations, data[0]))
+			f.write("[Keccak] Calculation per hash took {}ns\n\n".format(int(1000000000*data[1])))
+			f.close()
+		else:
+			print("[Keccak] Calculation of {} hashes took {}s".format(iterations, data[0]))
+			print("[Keccak] Calculation per hash took {}ns\n".format(int(1000000000*data[1])))
+	return(keccak_init(False, iterations))
 
 if __name__ == "__main__":
-	hashes = test_keccak_time()
-	test_collisions(hashes)
-	evaluate_probability(hashes)
-	evaluate_similarity(hashes, 16)
-	plot(bit_histogram(hashes),"keccak_bit_histogram")
-	plot(bucket_histogram(hashes,0xFFFF),"keccak_bucket_histogram")
-	hashes = test_hash_time()
-	test_collisions(hashes)
-	evaluate_probability(hashes)
-	evaluate_similarity(hashes, 16)
-	plot(bit_histogram(hashes),"squash_bit_histogram")
-	plot(bucket_histogram(hashes,0xFFFF),"squash_bucket_histogram")
+	keccak, squash, iterations, time, collisions, probability, similarity, bit, bucket, write, plot, out = init()
+
+	open(result_path("testresults.txt",out),"w").write('')
+	if keccak:
+		hashes = test_keccak_time(write=write, time=time, iterations=iterations, out=out)
+		if collisions:
+			test_collisions(hashes, write=write, out=out)
+		if probability:
+			evaluate_probability(hashes, write=write, out=out)
+		if similarity:
+			evaluate_similarity(hashes, 16, write=write, out=out)
+		if bucket:
+			plot_data(data=bucket_histogram(hashes,0xFFFF),name="keccak_bucket_histogram", plot=plot, out=out)
+		if bit:
+			plot_data(data=bit_histogram(hashes),name="keccak_bit_histogram", plot=plot, out=out)
+	if squash:
+		hashes = test_hash_time(write=write, time=time, iterations=iterations, out=out)
+		if collisions:
+			test_collisions(hashes, write=write, out=out)
+		if probability:
+			evaluate_probability(hashes, write=write, out=out)
+		if similarity:
+			evaluate_similarity(hashes, 16, write=write, out=out)
+		if bucket:
+			plot_data(data=bucket_histogram(hashes,0xFFFF),name="squash_bucket_histogram", plot=plot, out=out)
+		if bit:
+			plot_data(data=bit_histogram(hashes),name="squash_bit_histogram", plot=plot, out=out)
--- documents/proof_of_concept/utils.py
+++ documents/proof_of_concept/utils.py
@@ -1,6 +1,88 @@
 from hash import hash
-import time
+import time as time_module
 import hashlib
+import sys
+import os.path
+
+def parse_args():
+	keccak = ("--keccak" in sys.argv) or ("-k" in sys.argv)
+	squash = ("--squash" in sys.argv) or ("-s" in sys.argv)
+	iterations = ("--iterations" in sys.argv) or ("-i" in sys.argv)
+
+	time = ("--time" in sys.argv) or ("-t" in sys.argv)
+	collisions = ("--collisions" in sys.argv) or ("-c" in sys.argv)
+	probability = ("--probability" in sys.argv) or ("-p" in sys.argv)
+	similarity = ("--similarity" in sys.argv) or ("-S" in sys.argv)
+	bit = ("--bit" in sys.argv) or ("-b" in sys.argv)
+	bucket = ("--bucket" in sys.argv) or ("-B" in sys.argv)
+
+	write = ("--write" in sys.argv) or ("-w" in sys.argv)
+	plot = ("--plot" in sys.argv) or ("-P" in sys.argv)
+	out = ("--out" in sys.argv) or ("-o" in sys.argv)
+
+	help = ("--help" in sys.argv) or ("-h" in sys.argv)
+
+	if help or len(sys.argv) == 1:
+		show_help()
+	if iterations:
+		for i in range(len(sys.argv)):
+			if sys.argv[i] == "-i" or sys.argv[i] == "--iterations":
+				try:
+					iterations = int(sys.argv[i+1])
+					break
+				except:
+					print("Please enter a valid iteration number.")
+					break
+	if out:
+		for i in range(len(sys.argv)):
+			if sys.argv[i] == "-o" or sys.argv[i] == "--out":
+				try:
+					out = str(sys.argv[i+1])
+					if not(out[0] == '-'):
+						break
+					raise Exception('')
+				except:
+					print("Please enter a valid output directory.")
+					break
+	else:
+		out = "results"
+	return([keccak, squash, iterations, time, collisions, probability, similarity, bit, bucket, write, plot, out])
+			
+
+def show_help():
+	print("Algorithm options")
+	print("-k, --keccak              Perform tests on keccak")
+	print("-s, --squash              Perform tests on squash")
+	print("-i, --iterations          Set the number of iterations")
+	print('')
+	print("Test options")
+	print("-t, --time                Enable runtime testing")
+	print("-c, --collisions          Enable collision testing")
+	print("-p, --probability         Enable testing of value probabilities")
+	print("-S, --similarity          Enable testing of hash similarities")
+	print("-b, --bit                 Enable bit histogram")
+	print("-B, --bucket              Enable bucket histogram")
+	print('')
+	print("Output options")
+	print("-w, --write               Write to a file instead of console outputs")
+	print("-P, --plot                Show plots instead of writing them")
+	print("-o, --out                 Specify out directory")
+	print('')
+	print("-h, --help                Display this help message")
+	exit()
+
+def plot_data(data, name, plot, out):
+	import matplotlib.pyplot as plt	
+	plt.clf()
+	plt.scatter(data[1],data[0],s=0.2)
+	if plot:
+		plot.show(block=True)
+	else:
+		plt.savefig(result_path(name+".svg", out))
+		plt.savefig(result_path(name+".png", out))
+
+def result_path(name, folder):
+	return(os.path.join(os.path.dirname(os.path.realpath(__file__))+'/'+folder+'/', name))
 
 def padr(data, lenght):
 	data = str(data)
@@ -8,35 +90,33 @@ def padr(data, lenght):
 
 def squash_iterate(seed, iterations):
 	hashes = []
-	grand = iterations//256
-	_seed = ""
-	for i in range(1, grand+1):
-		_seed = seed[:-i]
-		for j in range(256):
-			hashes.append(hex(int.from_bytes(hash(_seed + (chr(j) * i).encode()),byteorder="big"))[2:])
+	grand = iterations//128
+	_seed = seed[:-grand]
+	for i in range(1,1+grand):
+		cur = grand-i
+		for j in range(1, 129):
+			hashes.append(hash(_seed + (chr(j)*i+'\x00'*cur).encode()).hex())
 	hashes = [h + '0' * (-(len(h))&0x1F) for h in hashes]
 	return hashes
 
 def squash_test_time(seed, iterations):
-	hash_value = hashlib.sha3_256(str(time.time()).encode()).digest()
+	hash_value = hashlib.sha3_256(str(time_module.time()).encode()).digest()
 	_seed = seed[:-32]
-	ctime = time.time()
+	ctime = time_module.time()
 	for i in range(iterations):
 			hash_value = hash(_seed + hash_value)
-	return (time.time() - ctime)
+	return (time_module.time() - ctime)
 
-def squash_init():
+def squash_init(time, iterations):
 	seed = bytes.fromhex(open("hex.txt","r").read()[:-1])
 
-	iterations = 0x100000 # 2**20
-	_time = squash_test_time(seed, iterations)
-	_per_hash = _time / iterations
-
-	iterations = 0x10000  # 2**16
-	_hashes = squash_iterate(seed, iterations)
-
-	return([_hashes,_time,_per_hash])
-
+	if time:
+		_time = squash_test_time(seed, iterations)
+		_per_hash = _time / iterations
+		return([_time,_per_hash])
+	else:
+		_hashes = squash_iterate(seed, iterations)
+		return(_hashes)
 
 
 
@@ -48,21 +128,29 @@ def keccak_iterate(seed, iterations):
 	return(_hashes)
 
 def keccak_test_time(seed, iterations):
-	ctime = time.time()
+	ctime = time_module.time()
 	for i in range(iterations):
 		seed = hashlib.sha3_256(seed).digest()
-	_time = time.time() - ctime
+	_time = time_module.time() - ctime
 	return(_time)
 
-def keccak_init():
-	seed = hashlib.sha3_256(str(time.time()).encode()).digest()
+def keccak_init(time, iterations):
+	seed = hashlib.sha3_256(str(time_module.time()).encode()).digest()
+	
+	if time:
+		_time = keccak_test_time(seed, iterations)
+		_per_hash = _time / iterations
+		return([_time,_per_hash])
+	else:
+		_hashes = keccak_iterate(seed, iterations)
+		return(_hashes)
 
-	iterations = 0x10000000 # 2**28
-	_time = keccak_test_time(seed, iterations)
-	_per_hash = _time / iterations
 
-	iterations = 0x10000    # 2**16
-	_hashes = keccak_iterate(seed, iterations)
-
-	return([_hashes,_time,_per_hash])
+def init():
+	keccak, squash, iterations, time, collisions, probability, similarity, bit, bucket, write, plot, out = parse_args()
+	try:
+		os.mkdir(result_path("",out))
+	except:
+		pass
+	return([keccak, squash, iterations, time, collisions, probability, similarity, bit, bucket, write, plot, out])
 	
