f853088f402defdf2582f09f3b49078475b1b09e
ClashLuke <luke@clashproject.com>
2019-02-27 11:00:23 +0100

    Rot32 -> Rot64

--- documents/proof_of_concept/hash.py
+++ documents/proof_of_concept/hash.py
@@ -16,21 +16,20 @@ def pad_classic(data, lenght, placeholder):
 def pad_encode(data, lenght, placeholder):
 	return (data + (placeholder * (-(len(data))&lenght)).encode())
 
-def ror(val, r_bits, max_bits):
-    return(((val & (2**max_bits-1)) >> r_bits%max_bits) | (val << (max_bits-(r_bits%max_bits)) & (2**max_bits-1)))
+def ror(val, r_bits):
+    return(((val & 0xFFFFFFFFFFFFFFFF) >> r_bits) | ((val << (0x3F-r_bits)) & 0xFFFFFFFFFFFFFFFF))
 
 def hash(data):
-	crc = ["","","",""]	
+	crc = ["","",""]	
 	crc[0] = pad_classic(hex(crc32(data))[2:], 0x7, '0')
 	bits1 = (read(data, int(crc[0][0:4],16))+read(data, int(crc[0][4:],16)))
 
 	crc[1] = pad_classic(hex(crc32(bytes.fromhex(crc[0])+bits1))[2:], 0x7, '0')
-	crc[2] = pad_classic(hex((int(crc[0],16) + int(crc[1],16))&0xFFFFFFFF)[2:], 0x7, '0')
-	crc[3] = pad_classic(hex(ror(int(crc[0],16), int(crc[1],16)&0x7, 32))[2:], 0x7, '0')
+	crc[2] = pad_classic(hex(ror(int(crc[0]+pad_classic(hex((int(crc[0],16) + int(crc[1],16))&0xFFFFFFFF)[2:], 0x7, '0'),16), int(crc[1],16)&0x3F))[2:], 0x7, '0')
 	bits2 = bytes.fromhex(''.join(crc))
 
 	if int((int(crc[0],16) / int(crc[1],16)))&1:
 		bits3 = read(data, int(crc[2][0:4],16))
 	else:
-		bits3 = read(data, int(crc[2][4:],16))
+		bits3 = read(data, int(crc[2][4:8],16))
 	return(bytes.fromhex(encrypt(bits3, bits2, bits1).hex()))
