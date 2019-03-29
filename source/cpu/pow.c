521c241d857cfefb2ed7d2ae34bf57cf7ff585d3
ClashLuke <luke@clashproject.com>
2019-03-29 23:07:12 +0100

    Disallow small-dataset-exploit

--- source/cpu/pow.c
+++ source/cpu/pow.c
@@ -14,7 +14,7 @@
 #define HASH_BYTES      32                 // hash length in bytes
 #define CACHE_ROUNDS    4                  // number of rounds in cache production
 #define EPOCH_LENGTH    60                 // blocks per epoch
-#define DATASET_PARENTS 256                // number of hashes before calculating dataset entry
+#define DATASET_PARENTS 16                // number of hashes before calculating dataset entry
 // Assuming 4 blocks per second, an epoch estimates 15 minutes
 
 void make_scratchpad(uint8_t* seed, uint8_t* scratchpad){
@@ -46,40 +46,49 @@ void calc_dataset_item(uint8_t* cache, uint32_t item_number, uint64_t* out){
 	uint32_t  mask     = 2097119; // Hashcount - 1 
 	uint32_t* cache_32 = (uint32_t*)cache; 
 	uint64_t  mix[4]   = {0};
-	uint8_t*  mix_8    = (uint8_t*)mix;
 	uint32_t* mix_32   = (uint32_t*)mix;
+	uint32_t  x        = 0;
 	uint8_t   i        = 0;
 	item_number = item_number >> 2;
-	mix_32[0] = cache_32[(item_number  )%mask];
-	mix_32[1] = cache_32[(item_number+1)%mask];
-	mix_32[2] = cache_32[(item_number+2)%mask];
-	mix_32[3] = cache_32[(item_number+3)%mask];
-	mix_32[4] = cache_32[(item_number+4)%mask];
-	mix_32[5] = cache_32[(item_number+5)%mask];
-	mix_32[6] = cache_32[(item_number+6)%mask];
-	mix_32[7] = cache_32[(item_number+7)%mask];
+	mix_32[0]  = cache_32[(item_number  )%mask];
+	mix_32[1]  = cache_32[(item_number+1)%mask];
+	mix_32[2]  = cache_32[(item_number+2)%mask];
+	mix_32[3]  = cache_32[(item_number+3)%mask];
+	mix_32[4]  = cache_32[(item_number+4)%mask];
+	mix_32[5]  = cache_32[(item_number+5)%mask];
+	mix_32[6]  = cache_32[(item_number+6)%mask];
+	mix_32[7]  = cache_32[(item_number+7)%mask];
+	mix_32[0] ^= item_number;
+	mix_32[1] ^= item_number;
+	mix_32[2] ^= item_number;
+	mix_32[3] ^= item_number;
+	mix_32[4] ^= item_number;
+	mix_32[5] ^= item_number;
+	mix_32[6] ^= item_number;
+	mix_32[7] ^= item_number;
 	for(uint16_t j=0;j<DATASET_PARENTS;j++){
 		i = j&7;
-		mix_32[i  ] = crc32(cache_32[mix_32[i  ]%mask]);
-		mix_32[i^1] = crc32(cache_32[mix_32[i^1]%mask]);
-		mix_32[i^2] = crc32(cache_32[mix_32[i^2]%mask]);
-		mix_32[i^3] = crc32(cache_32[mix_32[i^3]%mask]);
-		mix_32[i^4] = crc32(cache_32[mix_32[i^4]%mask]);
-		mix_32[i^5] = crc32(cache_32[mix_32[i^5]%mask]);
-		mix_32[i^6] = crc32(cache_32[mix_32[i^6]%mask]);
-		mix_32[i^7] = crc32(cache_32[mix_32[i^7]%mask]);
+		x = j^item_number;
+		mix_32[i  ] = crc32(x^cache_32[mix_32[i  ]%mask]);
+		mix_32[i^1] = crc32(x^cache_32[mix_32[i^1]%mask]);
+		mix_32[i^2] = crc32(x^cache_32[mix_32[i^2]%mask]);
+		mix_32[i^3] = crc32(x^cache_32[mix_32[i^3]%mask]);
+		mix_32[i^4] = crc32(x^cache_32[mix_32[i^4]%mask]);
+		mix_32[i^5] = crc32(x^cache_32[mix_32[i^5]%mask]);
+		mix_32[i^6] = crc32(x^cache_32[mix_32[i^6]%mask]);
+		mix_32[i^7] = crc32(x^cache_32[mix_32[i^7]%mask]);
 	}
 	out[0]=mix[0]; out[1]=mix[1];
 	out[2]=mix[2]; out[3]=mix[3];
 }
 
 void calc_dataset(uint8_t* cache, uint64_t* out){
-	for(uint64_t i=0;i<536870912;i+=4){ // (1<<32)>>3
+	for(uint32_t i=0;i<536870912;i+=4){ // (1<<32)>>3
 		calc_dataset_item(cache, i, &out[i]);
 	}
 }
 
-void squash_pow_full(uint8_t* header, uint64_t nonce, uint8_t* dataset, uint8_t* result){
+void squash_pow_full(uint8_t* header, uint64_t nonce, uint64_t* dataset, uint8_t* result){
 	uint64_t  seed_64[4] = {0}; 
 	uint8_t*  seed       = (uint8_t*)seed_64; 
 	memcpy(&header[80], (char*)&nonce, 8);
