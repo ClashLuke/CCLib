832b445349b37f7df511118ee7836321b955e538
ClashLuke <luke@clashproject.com>
2019-06-24 12:51:06 +0200

    Drastically improved threads (+added config)

--- Mash/benchmark.c
+++ Mash/benchmark.c
@@ -10,6 +10,8 @@
 #include "hash.h"
 #include "pow.h"
 #include "error.h"
+#include "config.h"
+
 
 void benchmark_dataset_generation(uint8_t* seed, uint64_t* dataset){
 	char      buffer[65] = {0};
@@ -33,7 +35,7 @@ void benchmark_dataset_generation(uint8_t* seed, uint64_t* dataset){
 
 }
 
-uint64_t benchmark_mine(uint64_t* seed_64, uint8_t printing, uint8_t threads, uint64_t ITERATIONS){
+uint64_t benchmark_mine(uint64_t* seed_64, uint8_t printing, uint64_t ITERATIONS){
 	uint64_t  result_64[4] = {0};
 	uint8_t*  result       = (uint8_t*)result_64;
 	uint64_t  temp[4]      = {0};
@@ -55,47 +57,23 @@ uint64_t benchmark_mine(uint64_t* seed_64, uint8_t printing, uint8_t threads, ui
 	current_time = (uint32_t)time(NULL);
 	if(printing){
 		printf("\rBenchmarking: [%s]",buffer); fflush(stdout);
-		if(!threads){
-			for(uint8_t j=0;j<64;j++){
-				for(uint64_t i=0;i<ITERATIONS;i++){
-					mash_full(seed, dataset, result);
-					temp[0] ^= result_64[0]; temp[1] ^= result_64[1];
-					temp[2] ^= result_64[2]; temp[3] ^= result_64[3];
-					(*seed_64)++; seed_64[1]++; seed_64[2]++; seed_64[3]++;
-				}
-				buffer[j] = '#';
-				printf("\rBenchmarking: [%s]",buffer); fflush(stdout);
-			}
-		}else{
-			threads++;
-			for(uint8_t j=0;j<64;j++){
-				for(uint64_t i=0;i<ITERATIONS;i+=threads){
-					mash_full_multi(seed, dataset, threads, result);
-					temp[0] ^= result_64[0]; temp[1] ^= result_64[1];
-					temp[2] ^= result_64[2]; temp[3] ^= result_64[3];
-					(*seed_64)++; seed_64[1]++; seed_64[2]++; seed_64[3]++;
-				}
-				buffer[j] = '#';
-				printf("\rBenchmarking: [%s]",buffer); fflush(stdout);
-			}
-		}
-		printf("\r%*s\r",80,"");
-	} else {
-		if(!threads){
-			for(uint64_t i=0;i<iterations;i++){
+		for(uint8_t j=0;j<64;j++){
+			for(uint64_t i=0;i<ITERATIONS;i+=MEMORY_THREADS){
 				mash_full(seed, dataset, result);
 				temp[0] ^= result_64[0]; temp[1] ^= result_64[1];
 				temp[2] ^= result_64[2]; temp[3] ^= result_64[3];
 				(*seed_64)++; seed_64[1]++; seed_64[2]++; seed_64[3]++;
 			}
-		}else{
-			threads++;
-			for(uint64_t i=0;i<iterations;i+=threads){
-				mash_full_multi(seed, dataset, threads, result);
-				temp[0] ^= result_64[0]; temp[1] ^= result_64[1];
-				temp[2] ^= result_64[2]; temp[3] ^= result_64[3];
-				(*seed_64)++; seed_64[1]++; seed_64[2]++; seed_64[3]++;
-			}
+			buffer[j] = '#';
+			printf("\rBenchmarking: [%s]",buffer); fflush(stdout);
+		}
+		printf("\r%*s\r",80,"");
+	} else {
+		for(uint64_t i=0;i<iterations;i+=MEMORY_THREADS){
+			mash_full(seed, dataset, result);
+			temp[0] ^= result_64[0]; temp[1] ^= result_64[1];
+			temp[2] ^= result_64[2]; temp[3] ^= result_64[3];
+			(*seed_64)++; seed_64[1]++; seed_64[2]++; seed_64[3]++;
 		}
 	}
 	free(dataset);
@@ -106,7 +84,7 @@ uint64_t benchmark_mine(uint64_t* seed_64, uint8_t printing, uint8_t threads, ui
 	return 0;
 }
 
-uint64_t benchmark_validation(uint64_t* seed_64, uint8_t printing, uint8_t threads, uint64_t ITERATIONS){
+uint64_t benchmark_validation(uint64_t* seed_64, uint8_t printing, uint64_t ITERATIONS){
 	uint64_t  result_64[4] = {0};
 	uint8_t*  result       = (uint8_t*)result_64;
 	uint64_t  temp[4]      = {0};
@@ -124,47 +102,23 @@ uint64_t benchmark_validation(uint64_t* seed_64, uint8_t printing, uint8_t threa
 	current_time = (uint32_t)time(NULL);
 	if(printing){
 		printf("\rBenchmarking: [%s]",buffer); fflush(stdout);
-		if(!threads){
-			for(uint8_t j=0;j<64;j++){
-				for(uint64_t i=0;i<iterations;i++){
-					mash_light(seed, cache, result);
-					temp[0] ^= result_64[0]; temp[1] ^= result_64[1];
-					temp[2] ^= result_64[2]; temp[3] ^= result_64[3];
-					(*seed_64)++; seed_64[1]++; seed_64[2]++; seed_64[3]++;
-				}
-				buffer[j] = '#';
-				printf("\rBenchmarking: [%s]",buffer); fflush(stdout);
-			}
-		}else{
-			threads++;
-			for(uint8_t j=0;j<64;j++){
-				for(uint64_t i=0;i<iterations;i+=threads){
-					mash_light_multi(seed, cache, threads, result);
-					temp[0] ^= result_64[0]; temp[1] ^= result_64[1];
-					temp[2] ^= result_64[2]; temp[3] ^= result_64[3];
-					(*seed_64)++; seed_64[1]++; seed_64[2]++; seed_64[3]++;
-				}
-				buffer[j] = '#';
-				printf("\rBenchmarking: [%s]",buffer); fflush(stdout);
-			}
-		}
-		printf("\r%*s\r",80,"");
-	} else {
-		if(!threads){
-			for(uint64_t i=0;i<ITERATIONS;i++){
+		for(uint8_t j=0;j<64;j++){
+			for(uint64_t i=0;i<iterations;i+=MEMORY_THREADS){
 				mash_light(seed, cache, result);
 				temp[0] ^= result_64[0]; temp[1] ^= result_64[1];
 				temp[2] ^= result_64[2]; temp[3] ^= result_64[3];
 				(*seed_64)++; seed_64[1]++; seed_64[2]++; seed_64[3]++;
 			}
-		}else{
-			threads++;
-			for(uint64_t i=0;i<ITERATIONS;i+=threads){
-				mash_light_multi(seed, cache, threads, result);
-				temp[0] ^= result_64[0]; temp[1] ^= result_64[1];
-				temp[2] ^= result_64[2]; temp[3] ^= result_64[3];
-				(*seed_64)++; seed_64[1]++; seed_64[2]++; seed_64[3]++;
-			}
+			buffer[j] = '#';
+			printf("\rBenchmarking: [%s]",buffer); fflush(stdout);
+		}
+		printf("\r%*s\r",80,"");
+	} else {
+		for(uint64_t i=0;i<ITERATIONS;i+=MEMORY_THREADS){
+			mash_light(seed, cache, result);
+			temp[0] ^= result_64[0]; temp[1] ^= result_64[1];
+			temp[2] ^= result_64[2]; temp[3] ^= result_64[3];
+			(*seed_64)++; seed_64[1]++; seed_64[2]++; seed_64[3]++;
 		}
 	}
 	free(cache);
@@ -179,25 +133,23 @@ uint64_t benchmark_validation(uint64_t* seed_64, uint8_t printing, uint8_t threa
 
 int main(int argc, char *argv[]){
 	uint8_t   printing   = argc>1?atoi(argv[1]):0;
-	uint8_t   threads    = argc>2?atoi(argv[2]):1;
-	uint8_t   iterShifts = argc>3?atoi(argv[3]):20;
-	uint32_t  seed       = argc>4?atoi(argv[4]):0x89ABCDEF;
+	uint8_t   iterShifts = argc>2?atoi(argv[2]):20;
+	uint32_t  seed       = argc>3?atoi(argv[3]):0x89ABCDEF;
 	uint32_t  seed_32[8] = {0};
 	uint64_t* seed_64    = (uint64_t*)seed_32;
 	uint64_t  iterations = 1;
-	threads--;
 	srand(seed);
 	for(uint8_t i=0; i<iterShifts; i++) iterations<<=1;
 	for(uint8_t i=0;i<8;i++) seed_32[i] = rand();
 	printf("\e[?25l"); // Hide cursor
 	printf("Parameters\n");
 	printf("\tProgressbar: %s\n", printing?"yes":"no");
-	printf("\tThreads: %u\n", 1+threads);
+	printf("\tMemory-Threads: %u\n", MEMORY_THREADS);
 	printf("\tSeed: %08x\n", seed);
 	printf("Mining\n");
-	benchmark_mine(seed_64, printing, threads, iterations);
+	benchmark_mine(seed_64, printing, iterations);
 	printf("Validation\n");
-	benchmark_validation(seed_64, printing, threads, iterations);
+	benchmark_validation(seed_64, printing, iterations);
 	printf("\e[?25h");
 	return 1;
 }
---
+++ Mash/config.h
@@ -0,0 +1,3 @@
+#define ACCESS_ROUNDS 256
+#define MEMORY_THREADS 256 // 256 is the absolute maximum
+			   // with 256 memory threads, 32kb of L1 cache are used for storage
