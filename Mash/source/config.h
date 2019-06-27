f4b6eef1ad29b5a33e9509f44db81486660a85b1
ClashLuke <luke@clashproject.com>
2019-06-27 20:31:05 +0200

    Changed mash config to achieve a setting suitable for Karbovanets

--- Mash/source/config.h
+++ Mash/source/config.h
@@ -1,3 +1,3 @@
-#define ACCESS_ROUNDS 10
-#define DATASET_PARENTS 1  // Determines dataset creation time and validation speed. The higher the slower.
+#define ACCESS_ROUNDS 64  // Determines speed of both full and light client. Linear slowdown for both (10 accessrounds -> 10x slower than 1 access round).
+#define DATASET_PARENTS 4   // Determines dataset creation time and validation speed. The higher the slower.
 //#define BENCHMARK           // XORing outputs of hash instead of checking for the best. Comment to check for best
