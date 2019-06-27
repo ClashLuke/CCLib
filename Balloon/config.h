3e68002911d56db54d1590398fe598f75dd37ac6
ClashLuke <luke@clashproject.com>
2019-06-27 19:06:09 +0200

    Formatted balloon config

--- Balloon/config.h
+++ Balloon/config.h
@@ -1,4 +1,4 @@
 #define SIZE (1<<21) // Bytes, stored in cache
 #define ITER (1<<20) // Iterations to move over the cache
-#define INPUT_SIZE 4 // In  8-byte blocks, maximum is 16
+#define INPUT_SIZE  4 // In  8-byte blocks, maximum is 16
 #define OUTPUT_SIZE 2 // In 16-byte blocks, maximum  is 4
