79f06d4b0a627cc52405d3fdf798875dabab07c4
ClashLuke <luke@clashproject.com>
2019-07-02 15:29:08 +0200

    updated pow header

--- Equihashimoto/source/pow.h
+++ Equihashimoto/source/pow.h
@@ -8,8 +8,9 @@
 
 void crc32p(uint32_t* in, uint32_t* out);
 void crc32i(uint32_t* in);
-void calcDatasetItem(uint8_t* seed, uint32_t itemNumber, uint64_t* out);
-void calcDataset(uint8_t* cache, uint64_t* out);
+uint32_t calcItem32(uint8_t* seed, uint32_t itemNumber);
+void calcDatasetItem8(uint8_t* seed, uint32_t itemNumber, uint64_t* out);
+void calcDataset(uint8_t* seed, uint64_t* out);
 void squash_pow_full(uint8_t* header, uint64_t nonce, uint64_t* dataset, uint8_t* result);
 void squash_pow_light(uint8_t* header, uint64_t nonce, uint8_t* cache, uint8_t* result);
 void get_seedhash(uint64_t block_number, uint8_t* seed);
