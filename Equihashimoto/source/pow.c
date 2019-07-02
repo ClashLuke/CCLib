525f920d05720e54e26acfb75824413ae9baba62
ClashLuke <luke@clashproject.com>
2019-07-02 02:58:33 +0200

    calcDatasetItem to calcDatasetItem8 for generation

--- Equihashimoto/source/pow.c
+++ Equihashimoto/source/pow.c
@@ -117,12 +117,6 @@ void calcDatasetItem(uint8_t* seed, uint32_t itemNumber, uint64_t* out){
 	memcpy(out, mix, 128);
 }
 
-void calcDataset(uint8_t* seed, uint64_t* out){
-	uint32_t* seed32 = (uint32_t*)seed;
-	for(uint32_t i=0;i<536870912;i+=16){ // (1<<32)>>3
-		calcDatasetItem(seed, i, &out[i]);
-		(*seed32)++; seed32[1]++; seed32[2]++; seed32[3]++;
-		seed32[4]++; seed32[5]++; seed32[6]++; seed32[7]++;
 uint32_t calcItem32(uint8_t* data, uint32_t itemNumber){
 	uint8_t   item[128] = {0};
 	uint32_t* item_32  = (uint32_t*)item;
@@ -154,4 +148,79 @@ uint32_t calcItem32(uint8_t* data, uint32_t itemNumber){
 			out0  |= out1;
 		}
 	}
+	return out0;
+}
+
+void calcDatasetItem8(uint8_t* seed, uint32_t itemNumber, uint64_t* out){
+	uint64_t mix[128]  = {0};
+	uint8_t* mix8      = (uint8_t*)mix;
+	*mix     = itemNumber; mix[  2] = itemNumber; mix[  4] = itemNumber; mix[  6] = itemNumber;
+	mix[  8] = itemNumber; mix[ 10] = itemNumber; mix[ 12] = itemNumber; mix[ 14] = itemNumber;
+	mix[ 16] = itemNumber; mix[ 18] = itemNumber; mix[ 20] = itemNumber; mix[ 22] = itemNumber;
+	mix[ 24] = itemNumber; mix[ 26] = itemNumber; mix[ 28] = itemNumber; mix[ 30] = itemNumber;
+	mix[ 32] = itemNumber; mix[ 34] = itemNumber; mix[ 36] = itemNumber; mix[ 38] = itemNumber;
+	mix[ 40] = itemNumber; mix[ 42] = itemNumber; mix[ 44] = itemNumber; mix[ 46] = itemNumber;
+	mix[ 48] = itemNumber; mix[ 50] = itemNumber; mix[ 52] = itemNumber; mix[ 54] = itemNumber;
+	mix[ 56] = itemNumber; mix[ 58] = itemNumber; mix[ 60] = itemNumber; mix[ 62] = itemNumber;
+	mix[ 64] = itemNumber; mix[ 66] = itemNumber; mix[ 68] = itemNumber; mix[ 70] = itemNumber;
+	mix[ 72] = itemNumber; mix[ 74] = itemNumber; mix[ 76] = itemNumber; mix[ 78] = itemNumber;
+	mix[ 80] = itemNumber; mix[ 82] = itemNumber; mix[ 84] = itemNumber; mix[ 86] = itemNumber;
+	mix[ 88] = itemNumber; mix[ 90] = itemNumber; mix[ 92] = itemNumber; mix[ 94] = itemNumber;
+	mix[ 96] = itemNumber; mix[ 98] = itemNumber; mix[100] = itemNumber; mix[102] = itemNumber;
+	mix[104] = itemNumber; mix[106] = itemNumber; mix[108] = itemNumber; mix[110] = itemNumber;
+	mix[112] = itemNumber; mix[114] = itemNumber; mix[116] = itemNumber; mix[118] = itemNumber;
+	mix[120] = itemNumber; mix[122] = itemNumber; mix[124] = itemNumber; mix[126] = itemNumber;
+	mix[ 16]+=16; mix[ 18]+=16; mix[ 20]+=16; mix[ 22]+=16;
+	mix[ 24]+=16; mix[ 26]+=16; mix[ 28]+=16; mix[ 30]+=16;
+	mix[ 32]+=32; mix[ 34]+=32; mix[ 36]+=32; mix[ 38]+=32;
+	mix[ 40]+=32; mix[ 42]+=32; mix[ 44]+=32; mix[ 46]+=32;
+	mix[ 48]+=48; mix[ 50]+=48; mix[ 52]+=48; mix[ 54]+=48;
+	mix[ 56]+=48; mix[ 58]+=48; mix[ 60]+=48; mix[ 62]+=48;
+	mix[ 64]+=64; mix[ 66]+=64; mix[ 68]+=64; mix[ 70]+=64;
+	mix[ 72]+=64; mix[ 74]+=64; mix[ 76]+=64; mix[ 78]+=64;
+	mix[ 80]+=80; mix[ 82]+=80; mix[ 84]+=80; mix[ 86]+=80;
+	mix[ 88]+=80; mix[ 90]+=80; mix[ 92]+=80; mix[ 94]+=80;
+	mix[ 96]+=96; mix[ 96]+=96; mix[ 98]+=96; mix[100]+=96;
+	mix[104]+=96; mix[106]+=96; mix[108]+=96; mix[110]+=96;
+	mix[112]+=112; mix[114]+=112; mix[116]+=112; mix[118]+=112;
+	mix[120]+=112; mix[122]+=112; mix[124]+=112; mix[126]+=112;
+	aes(mix8,        seed       ); aes(&mix8[  16], &seed[  16]);
+	aes(&mix8[  32], mix8       ); aes(&mix8[  48], &mix8[  16]);
+	aes(&mix8[  64], seed       ); aes(&mix8[  80], &seed[  16]);
+	aes(&mix8[  96], &mix8[  64]); aes(&mix8[ 112], &mix8[  80]);
+	aes(&mix8[ 128], seed       ); aes(&mix8[ 144], &seed[  16]);
+	aes(&mix8[ 160], &mix8[ 128]); aes(&mix8[ 176], &mix8[ 144]);
+	aes(&mix8[ 192], seed       ); aes(&mix8[ 208], &seed[  16]);
+	aes(&mix8[ 224], &mix8[ 192]); aes(&mix8[ 240], &mix8[ 208]);
+	aes(&mix8[ 256], seed       ); aes(&mix8[ 272], &seed[  16]);
+	aes(&mix8[ 288], &mix8[ 256]); aes(&mix8[ 304], &mix8[ 272]);
+	aes(&mix8[ 320], seed       ); aes(&mix8[ 336], &seed[  16]);
+	aes(&mix8[ 352], &mix8[ 320]); aes(&mix8[ 368], &mix8[ 336]);
+	aes(&mix8[ 384], seed       ); aes(&mix8[ 400], &seed[  16]);
+	aes(&mix8[ 416], &mix8[ 384]); aes(&mix8[ 432], &mix8[ 400]);
+	aes(&mix8[ 448], seed       ); aes(&mix8[ 464], &seed[  16]);
+	aes(&mix8[ 480], &mix8[ 448]); aes(&mix8[ 496], &mix8[ 464]);
+	aes(&mix8[ 512], seed       ); aes(&mix8[ 528], &seed[  16]);
+	aes(&mix8[ 544], &mix8[ 512]); aes(&mix8[ 560], &mix8[ 528]);
+	aes(&mix8[ 576], seed       ); aes(&mix8[ 592], &seed[  16]);
+	aes(&mix8[ 608], &mix8[ 576]); aes(&mix8[ 624], &mix8[ 592]);
+	aes(&mix8[ 640], seed       ); aes(&mix8[ 656], &seed[  16]);
+	aes(&mix8[ 672], &mix8[ 640]); aes(&mix8[ 688], &mix8[ 656]);
+	aes(&mix8[ 704], seed       ); aes(&mix8[ 720], &seed[  16]);
+	aes(&mix8[ 736], &mix8[ 704]); aes(&mix8[ 752], &mix8[ 720]);
+	aes(&mix8[ 768], seed       ); aes(&mix8[ 784], &seed[  16]);
+	aes(&mix8[ 800], &mix8[ 768]); aes(&mix8[ 816], &mix8[ 784]);
+	aes(&mix8[ 832], seed       ); aes(&mix8[ 848], &seed[  16]);
+	aes(&mix8[ 864], &mix8[ 832]); aes(&mix8[ 880], &mix8[ 848]);
+	aes(&mix8[ 896], seed       ); aes(&mix8[ 912], &seed[  16]);
+	aes(&mix8[ 928], &mix8[ 896]); aes(&mix8[ 944], &mix8[ 912]);
+	aes(&mix8[ 960], seed       ); aes(&mix8[ 976], &seed[  16]);
+	aes(&mix8[ 992], &mix8[ 960]); aes(&mix8[1008], &mix8[ 976]);
+	memcpy(out, mix, 1024);
+}
+
+void calcDataset(uint8_t* seed, uint64_t* out){
+	for(uint32_t i=0;i<536870912;i+=128) // (1<<32)>>3
+		calcDatasetItem8(seed, i, &out[i]);
+	
 }
