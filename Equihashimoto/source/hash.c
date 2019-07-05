1d40bb2b76ca455294929b9c8f060eaa1223d9c4
ClashLuke <luke@clashproject.com>
2019-07-05 19:03:45 +0200

    Reincreased number of "ROUNDS > X" loops, added loops for ROUNDS <= X, moved loops

--- Equihashimoto/source/hash.c
+++ Equihashimoto/source/hash.c
@@ -23,54 +23,60 @@ void mash_full(uint8_t* data, uint8_t* dataset, uint8_t* out){
 		// read 64bit and process 32 elements without reading new data?
 		// use 64bit instead of 32bit blocks?
 		*item = *(uint32_t*)&dataset[i];
-		for(uint32_t j=1+i; j<MAX32; j++){
 			#if ROUNDS > 2
+			for(uint32_t j=1+i; j<MAX32; j++){
 			item[1] = *item ^ *(uint32_t*)&dataset[j];
-			for(uint32_t k=1+j; k<MAX32; k+=1024){
 			#if ROUNDS > 3
+			for(uint32_t k=1+j; k<MAX32; k++){
 			item[2] = item[1] ^ *(uint32_t*)&dataset[k];
-			for(uint32_t l=1+k; l<MAX32; l+=1024){
 			#if ROUNDS > 4
+			for(uint32_t l=1+k; l<MAX32; l++){
 			item[3] = item[2] ^ *(uint32_t*)&dataset[l];
-			for(uint32_t m=1+l; m<MAX32; m+=1024){
 			#if ROUNDS > 5
+			for(uint32_t m=1+l; m<MAX32; m++){
 			item[4] = item[3] ^ *(uint32_t*)&dataset[m];
-			for(uint32_t n=1+m; n<MAX32; n+=1024){
 			#if ROUNDS > 6
+			for(uint32_t n=1+m; n<MAX32; n++){
 			item[5] = item[4] ^ *(uint32_t*)&dataset[n];
-			for(uint32_t o=1+n; o<MAX32; o+=1024){
 			#if ROUNDS > 7
+			for(uint32_t o=1+n; o<MAX32; o++){
 			item[6] = item[5] ^ *(uint32_t*)&dataset[o];
 			for(uint32_t p=1+o; p<MAX32; p+=1024){
 			memcpy(cache, &dataset[p], 1028);
 			for(uint16_t a=0; a<1024; a++){
 			if(item[6] == *(uint32_t*)&cache[a]){
 			#else
+			for(uint32_t o=1+n; o<MAX32; o+=1024){
 			memcpy(cache, &dataset[o], 1028);
 			for(uint16_t a=0; a<1024; a++){
 			if(item[5] == *(uint32_t*)&cache[a]){
 			#endif
 			#else
+			for(uint32_t n=1+m; n<MAX32; n+=1024){
 			memcpy(cache, &dataset[n], 1028);
 			for(uint16_t a=0; a<1024; a++){
 			if(item[4] == *(uint32_t*)&cache[a]){
 			#endif
 			#else
+			for(uint32_t m=1+l; m<MAX32; m+=1024){
 			memcpy(cache, &dataset[m], 1028);
 			for(uint16_t a=0; a<1024; a++){
 			if(item[3] == *(uint32_t*)&cache[a]){
 			#endif
 			#else
+			for(uint32_t l=1+k; l<MAX32; l+=1024){
 			memcpy(cache, &dataset[l], 1028);
 			for(uint16_t a=0; a<1024; a++){
 			if(item[2] == *(uint32_t*)&cache[a]){
 			#endif
 			#else
+			for(uint32_t k=1+j; k<MAX32; k+=1024){
 			memcpy(cache, &dataset[k], 1028);
 			for(uint16_t a=0; a<1024; a++){
 			if(item[1] == *(uint32_t*)&cache[a]){
 			#endif
 			#else
+			for(uint32_t j=1+i; j<MAX32; j+=1024){
 			memcpy(cache, &dataset[j], 1028);
 			for(uint16_t a=0; a<1024; a++){
 			if(*item   == *(uint32_t*)&dataset[a]){
