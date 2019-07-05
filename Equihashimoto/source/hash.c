8e90674b74dc6a5d27886f142f82637e15ee8e40
ClashLuke <luke@clashproject.com>
2019-07-05 18:41:53 +0200

    XOR instead of check for equality

--- Equihashimoto/source/hash.c
+++ Equihashimoto/source/hash.c
@@ -15,37 +15,49 @@
 void mash_full(uint8_t* data, uint8_t* dataset, uint8_t* out){
 	uint32_t* out32 = (uint32_t*)out;
 	uint64_t* dataset64 = (uint64_t*)dataset;
-	uint32_t  item = 0;
+	uint32_t  item[ROUNDS] = {0};
 	calcDataset(data, dataset64);
 	for(uint32_t i=0; i<MAX32; i++){
 		// read 64bit and process 32 elements without reading new data?
 		// use 64bit instead of 32bit blocks?
+		*item = *(uint32_t*)&dataset[i];
 		for(uint32_t j=1+i; j<MAX32; j++){
-			item = *(uint32_t*)&dataset[i];
-			if(item==*(uint32_t*)&dataset[j]){
 			#if ROUNDS > 2
+			item[1] = *item ^ *(uint32_t*)&dataset[j];
 			for(uint32_t k=1+j; k<MAX32; k++){
-			if(item==*(uint32_t*)&dataset[k]){
 			#if ROUNDS > 3
+			item[2] = item[1] ^ *(uint32_t*)&dataset[k];
 			for(uint32_t l=1+k; l<MAX32; l++){
-			if(item==*(uint32_t*)&dataset[l]){
 			#if ROUNDS > 4
+			item[3] = item[2] ^ *(uint32_t*)&dataset[l];
 			for(uint32_t m=1+l; m<MAX32; m++){
-			if(item==*(uint32_t*)&dataset[m]){
 			#if ROUNDS > 5
+			item[4] = item[3] ^ *(uint32_t*)&dataset[m];
 			for(uint32_t n=1+m; n<MAX32; n++){
-			if(item==*(uint32_t*)&dataset[n]){
 			#if ROUNDS > 6
+			item[5] = item[4] ^ *(uint32_t*)&dataset[n];
 			for(uint32_t o=1+n; o<MAX32; o++){
-			if(item==*(uint32_t*)&dataset[o]){
 			#if ROUNDS > 7
+			item[6] = item[5] ^ *(uint32_t*)&dataset[o];
 			for(uint32_t p=1+o; p<MAX32; p++){
-			if(item==*(uint32_t*)&dataset[p]){
+			if(item[6] == *(uint32_t*)&dataset[p]){
+			#else
+			if(item[5] == *(uint32_t*)&dataset[o]){
 			#endif
+			#else
+			if(item[4] == *(uint32_t*)&dataset[n]){
 			#endif
+			#else
+			if(item[3] == *(uint32_t*)&dataset[m]){
 			#endif
+			#else
+			if(item[2] == *(uint32_t*)&dataset[l]){
 			#endif
+			#else
+			if(item[1] == *(uint32_t*)&dataset[k]){
 			#endif
+			#else
+			if(*item   == *(uint32_t*)&dataset[j]){
 			#endif
 			*out32 = i;
 			out32[1] = j;
@@ -70,28 +82,23 @@ void mash_full(uint8_t* data, uint8_t* dataset, uint8_t* out){
 			return;
 			#if ROUNDS > 2
 			}
-			}
 			#if ROUNDS > 3
-			}
 			}
 			#if ROUNDS > 4
 			}
-			}
 			#if ROUNDS > 5
-			}
 			}
 			#if ROUNDS > 6
 			}
-			}
 			#if ROUNDS > 7
 			}
+			#endif
+			#endif
+			#endif
+			#endif
+			#endif
+			#endif
 			}
-			#endif
-			#endif
-			#endif
-			#endif
-			#endif
-			#endif
 			}
 		}
 	}
