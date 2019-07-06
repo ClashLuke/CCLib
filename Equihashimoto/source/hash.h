a8ab0068aab0eed5044d63aa91691765f10e7955
ClashLuke <luke@clashproject.com>
2019-07-06 19:53:53 +0200

    Added hash.c changes to header

--- Equihashimoto/source/hash.h
+++ Equihashimoto/source/hash.h
@@ -7,7 +7,7 @@
 #define MASH_H
 
 
-void mash_full(uint8_t* data, uint8_t* dataset, uint8_t* out);
-uint8_t mash_light(uint8_t* data);
+void mash_full(uint8_t* data, uint8_t* dataset, uint64_t item, uint64_t difficulty, uint32_t* out);
+uint8_t mash_light(uint8_t* data, uint64_t difficulty);
 
 #endif
