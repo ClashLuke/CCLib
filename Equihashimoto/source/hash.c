897795510522792d61c66a20c6a0b0e64a5c97f1
ClashLuke <luke@clashproject.com>
2019-07-07 23:59:23 +0200

    2x diff instead of equality and diff

--- Equihashimoto/source/hash.c
+++ Equihashimoto/source/hash.c
@@ -11,34 +11,31 @@
 #include "config.h"
 
 
-void mash_full(const uint8_t* data, uint8_t* dataset){
-	uint64_t  j = 0;
-	uint64_t  k = 0;
+void mash_full(uint8_t* data, uint8_t* dataset){
 	uint8_t   cache0[BLOCK+8] = {0};
 	uint8_t   cache1[BLOCK+8] = {0};
 	uint64_t  item      = *(uint64_t*)&data[64];  
 	uint32_t* item32    = (uint32_t*)&data[64]; 
 	const uint64_t diff = *(uint64_t*)&data[72];
 	calcDataset(data, dataset);
-	for(; j<ITEMS; j+=BLOCK){
-	memcpy(cache0, &dataset[j], BLOCK+8);
-	for(uint32_t a=0; a<BLOCK; a++){
-	if(*item32 == *(uint32_t*)&cache0[a]){
-	k = 0;
-	item ^= *(uint64_t*)&cache0[a];
-	for(; k<ITEMS; k+=BLOCK){
-	memcpy(cache1, &dataset[k], BLOCK+8);
-	for(uint32_t b=0; b<BLOCK; b++){
-	if((item ^ *(uint64_t*)&cache1[b]) < diff){
-	*item32   = j+a;
-	item32[1] = k+b;
-	return;
-	}
-	}
-	}	
-	item ^= *(uint64_t*)&cache0[a];
-	}
-	}
+	for(uint64_t j=0; j<ITEMS; j+=BLOCK){
+		memcpy((void*)cache0, (void*)&dataset[j], BLOCK+8);
+		for(uint32_t a=0; a<BLOCK; a++){
+			item ^= *(uint64_t*)&cache0[a];
+			if(item < diff){
+				for(uint64_t k=j+a+1; k<ITEMS; k+=BLOCK){
+					memcpy(cache1, &dataset[k], BLOCK+8);
+					for(uint32_t b=0; b<BLOCK; b++){
+						if((item ^ *(uint64_t*)&cache1[b]) < diff){
+							*item32   = j+a;
+							item32[1] = k+b;
+							return;
+						}
+					}
+				}	
+			}
+			item ^= *(uint64_t*)&cache0[a];
+		}
 	}
 }
 
