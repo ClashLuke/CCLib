17db57b679053302ba2c9aaad3d5587b3b0fbd5c
ClashLuke <luke@clashproject.com>
2019-07-07 02:06:36 +0200

    Added access-block size

--- Equihashimoto/source/config.h
+++ Equihashimoto/source/config.h
@@ -10,4 +10,16 @@
                            // may be changed to achieve a different
                            // dataset size.
 
+#define BLOCK  (1<<20)     // Size of blocks retrieved from memory.
+			   // Bigger blocks allow for less memory
+			   // accesses (less bound to memory latency)
+			   // of larger blocks (more bound to memory
+			   // bandwidth) yet also need more cache.
+			   // Specifically BLOCK*2+12 bytes are
+			   // required per thread.
+			   // Changing this will change the speed of
+			   // the algorithm but not its result. It may
+			   // be used to tune it to its ideal
+			   // parameters on a specific machine.
+
 #endif
--- Equihashimoto/source/hash.c
+++ Equihashimoto/source/hash.c
@@ -14,19 +14,19 @@
 void mash_full(uint8_t* data, uint8_t* dataset, uint64_t item, uint64_t difficulty, uint32_t* out){
 	uint64_t  j            = 0;
 	uint64_t  k            = 0;
-	uint8_t   cache0[8196] = {0};
-	uint8_t   cache1[8200] = {0};
+	uint8_t   cache0[BLOCK+4] = {0};
+	uint8_t   cache1[BLOCK+8] = {0};
 	uint32_t* item32       = (uint32_t*)&item; 
 	calcDataset(data, (uint64_t*)dataset);
-	for(; j<ITEMS; j+=8192){
-	memcpy(cache0, &dataset[j], 8196);
-	for(uint16_t a=0; a<8192; a++){
+	for(; j<ITEMS; j+=BLOCK){
+	memcpy(cache0, &dataset[j], BLOCK+4);
+	for(uint32_t a=0; a<BLOCK; a++){
 	if(*item32 == *(uint32_t*)&cache0[a]){
-	k = 0;
-	for(; k<ITEMS; k+=8192){
-	memcpy(cache1, &dataset[k], 8200);
-	for(uint16_t b=0; b<8192; b++){
-	if((item ^ *(uint64_t*)&cache1[b]) > difficulty){
+	k = j+a;
+	for(; k<ITEMS; k+=BLOCK){
+	memcpy(cache1, &dataset[k], BLOCK+8);
+	for(uint32_t b=0; b<BLOCK; b++){
+	if((item ^ *(uint64_t*)&cache1[b]) < difficulty){
 	*out   = j+a;
 	out[1] = k+b;
 	return;
