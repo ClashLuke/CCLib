7fbf6af4bdb6456c650c3cdbdc1c2195d6a738b6
ClashLuke <luke@clashproject.com>
2019-07-08 22:20:06 +0200

    Updated squash (2x speed)

--- Squash/readme.md
+++ Squash/readme.md
@@ -7,8 +7,6 @@ A fast and lightweight hashing algorithm with built-in ASIC resistance.
 	- [Speed](#Speed)
 	- [Compilation](#Compilation)
 - [Security](#Security)
-	- [Squash 0](#Squash_0)
-	- [Squash 1](#Squash_1)
 - [Conclusions](#Conclusions)
 
 ## General
@@ -16,7 +14,7 @@ A fast and lightweight hashing algorithm with built-in ASIC resistance.
 Tests and sample results can be found in the [Tests Branch](https://github.com/ClashLuke/CCLib/tree/testing). Head over there for further instructions.
 
 ### Speed
-On a [Xeon E3-1225v2](https://ark.intel.com/content/www/us/en/ark/products/65733/intel-xeon-processor-e3-1225-v2-8m-cache-3-20-ghz.html), the algorithm has an average speed of 58.5ns. For comparision, keccak takes about 800ns on average. Which results in 5.8 cpb for squash, 80 cpb for keccak - on an Ivy Bridge CPU.<br>
+On a [Xeon E3-1225v2](https://ark.intel.com/content/www/us/en/ark/products/65733/intel-xeon-processor-e3-1225-v2-8m-cache-3-20-ghz.html), the algorithm has an average speed of 33.0ns. For comparision, keccak takes about 800ns on average. Which results in 3.3 cpb for squash, 80 cpb for keccak - on an Ivy Bridge CPU.<br>
 On a [Raspberry Pi 3b+](https://www.raspberrypi.org/magpi/raspberry-pi-specs-benchmarks/), the calculation of one hash takes about 1534ns on average, which results in 33.5 cpb. For comparision, one keccak execution takes about 15467ns which results in 338 cpb.
 
 ### Compilation
@@ -24,35 +22,17 @@ To get the optimal speed, it is recommended to compile using the `-O3 -march=nat
 
 ## Security
 
-Squash 2 and 3 are variations of squash 1 with a larger scratchpad, hence we will we will focus on the squash variants 0 and 1 in here.
-
-### Squash_0
-
-#### Step 1 - CRC32
+### Step 1 - CRC32
 Assuming that there is a perfect CRC, a 32bit redundancy check (with random 32bit inputs) has 2^16 collisions. We calculate the CRC four times to have delays according to the inner-CPU delays, as seen [here](https://github.com/JuliaLang/julia/blob/master/src/crc32c.c#L111). Now 128bit of the input are processed, resulting in 128bit output with 64bit collision-resistance. Code: `crc_32[0] = crc32(data_32[0]);` CRCs are heavily optimised on ARM CPUs since they are an addition to the instructionset. They are relatively expensive for ASICs to implement, according to [this](https://www.slideshare.net/bschn2/the-rainforest-algorithm) post.
 
-#### Step 2 - Integer Math
+### Step 2 - Integer Math
 The next 128bit are calculated using two times two 64bit integer arithmetics. First, the next 64bit of input data are used to be added to the first two CRCs. Since an addition is a XOR with carry, it can be assumed that the first bit is a casual XOR, all following bits are a doubled XOR. This results in a slightly increased probability for high results - which, since the highest bit is cut off, does not matter. To even the first few bits out, a division of the previous data (3rd 64bit of the input, 1st 64bit of the CRC results) is added to the previous result using a XOR. The resulting expression looks like this `crc_64[2] = (data_64[2] + crc_64[0]) ^ (data_64[2] / crc_64[0]);`. This results in another 128bit with 64bit collision-resistance (assuming that the ADD operator is cryptographically insecure). While those 64bit operations are easy for a modern CPU, it is difficult for ASICs to implement those [2](https://www.slideshare.net/bschn2/the-rainforest-algorithm)
 
-#### Step 3 - Bit Moving
+### Step 3 - Bit Moving
 A bit reverse and a byteswap of existing data are additionally added, since those require one cycle but significantly increase GPU and ASIC resistance. None of these increases entropy nor collision resistance, but the byteswap is optimised for x86 and ARM instructionsets, which results in a slowdown for GPUs. Additionally the bit reverse is optimised in the ARM instructionset as [rbit](http://www.keil.com/support/man/docs/armasm/armasm_dom1361289889382.htm), which increases performance for low-power devices such as [ARM Server CPUs](https://www.arm.com/products/silicon-ip-cpu/neoverse/neoverse-n1) or smartphones.
 
-#### Step 4 - Rotation
+### Step 4 - Rotation
 Compared to the previous operations, the rotations are relatively cheap. `key[1][0] = (out_64[0]>>shift[0]) | (out_64[0]<<(shift[1]));` Two 64bit rotations in each direction are used to obtain keys to encrypt the previously generated 256bit with. Those are variable rotations, the number of bits to shift is obtained by reading from the first/last byte of the previous 256bit. Since those dont change fundamentally change data but instead are a fancy copy mechanism which provides additional protection against ASICs but cost nothing on a casual CPU [2](https://www.slideshare.net/bschn2/the-rainforest-algorithm).
 
-#### Step 5 - Encryption
+### Step 5 - Encryption
 The last step, an AES ECB encryption, is there to increase entropy, GPU resistance and ASIC resistance as seen [here](https://www.slideshare.net/bschn2/the-rainforest-algorithm). It does not fundamentally increase collision resistance or security. The encryption is performed using the first 128bit of the previous results (the CRC results) and encrypts them with the key obtained from the integer math. The code looks like this: `aes(out, (uint8_t*)key[0]);`. The same thing goes for the second 128bit encrypted with the rotated CRC.
-
-### Squash_1
-
-#### Differences to Squash 0
-Squash 1 has the same steps as Squash 0, just another step inserted after the first CRC and a slightly changed integer math.
-
-#### Step 1.5 - Scratchpad
-Right after the first step, four CRC32s, their data is taken to read from a read-only scratchpad. In Squash 1, the first 8bit are used to read from a 256 Byte scratchpad, Squash 2 uses the first 16bit of each CRC to read from a 64KiB scratchpad, Squash 3 uses the full 32bit of each CRC to read from a 4GiB scratchpad. The data obtained from Cache/RAM is then stored as the next 128bit of the data, like this `crc_32[4] = ((uint32_t*)&scratchpad[crc_8[ 0]])[0];`, so that there are two 128bit blocks totalling in 256bit. This step is solely used to force an attacker to store the (ideally pseudorandom) scratchpad. In case of Squash 3, there are iterations of reading, to have the hash be I/O bound instead of processing bound. Since this step heavily relies on the previous CRC, it cannot be better than the CRC32, which results in 64bit collision resistance for the read 128bit.
-
-#### Step 2 - Integer Math
-Since the second 128bit already are assigned, it would not be useful to reassign them, ignoring their data. Thats why the result of the two 64bit math operations is calculated using 128bit of the previous data `divr[0] = (data_64[2] + crc_64[2]) ^ (data_64[2] / crc_64[0]);` and then XORed with two 64bit blocks `out_64[0] = crc_64[0]^divr[0]; out_64[1] = crc_64[1]^divr[0];`. There are different blocks XORed than used for calculation to increase the variaty of results.
-
-## Conclusions
-To sum everything up, Squash is a hash with 128bit collision resistance, a high speed of 5.8 cycles per byte (Ivy Bridge, 4.0 cpb on Zen). In contrast to other hashes, Squash relies on 256bit inputs but is also able to provide a decent level of ASIC resistance. To further increase the ASIC resistance without decreasing the speeds below a [certain level](https://github.com/ethereum/wiki/wiki/Ethash-Design-Rationale), SquashPoW was introduced.
--- Squash/squash.c
+++ Squash/squash.c
@@ -6,10 +6,133 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include "aes.h"
 
-#define ACCESSES 256
 
+#if (!defined(__aarch64__) || !defined(__ARM_FEATURE_CRYPTO)) && (!defined(__x86_64__) || !defined(__AES__))
+static const uint8_t SBOX[256] = {
+	0x63,0x7c,0x77,0x7b,0xf2,0x6b,0x6f,0xc5,0x30,0x01,0x67,0x2b,0xfe,0xd7,0xab,0x76,
+	0xca,0x82,0xc9,0x7d,0xfa,0x59,0x47,0xf0,0xad,0xd4,0xa2,0xaf,0x9c,0xa4,0x72,0xc0,
+	0xb7,0xfd,0x93,0x26,0x36,0x3f,0xf7,0xcc,0x34,0xa5,0xe5,0xf1,0x71,0xd8,0x31,0x15,
+	0x04,0xc7,0x23,0xc3,0x18,0x96,0x05,0x9a,0x07,0x12,0x80,0xe2,0xeb,0x27,0xb2,0x75,
+	0x09,0x83,0x2c,0x1a,0x1b,0x6e,0x5a,0xa0,0x52,0x3b,0xd6,0xb3,0x29,0xe3,0x2f,0x84,
+	0x53,0xd1,0x00,0xed,0x20,0xfc,0xb1,0x5b,0x6a,0xcb,0xbe,0x39,0x4a,0x4c,0x58,0xcf,
+	0xd0,0xef,0xaa,0xfb,0x43,0x4d,0x33,0x85,0x45,0xf9,0x02,0x7f,0x50,0x3c,0x9f,0xa8,
+	0x51,0xa3,0x40,0x8f,0x92,0x9d,0x38,0xf5,0xbc,0xb6,0xda,0x21,0x10,0xff,0xf3,0xd2,
+	0xcd,0x0c,0x13,0xec,0x5f,0x97,0x44,0x17,0xc4,0xa7,0x7e,0x3d,0x64,0x5d,0x19,0x73,
+	0x60,0x81,0x4f,0xdc,0x22,0x2a,0x90,0x88,0x46,0xee,0xb8,0x14,0xde,0x5e,0x0b,0xdb,
+	0xe0,0x32,0x3a,0x0a,0x49,0x06,0x24,0x5c,0xc2,0xd3,0xac,0x62,0x91,0x95,0xe4,0x79,
+	0xe7,0xc8,0x37,0x6d,0x8d,0xd5,0x4e,0xa9,0x6c,0x56,0xf4,0xea,0x65,0x7a,0xae,0x08,
+	0xba,0x78,0x25,0x2e,0x1c,0xa6,0xb4,0xc6,0xe8,0xdd,0x74,0x1f,0x4b,0xbd,0x8b,0x8a,
+	0x70,0x3e,0xb5,0x66,0x48,0x03,0xf6,0x0e,0x61,0x35,0x57,0xb9,0x86,0xc1,0x1d,0x9e,
+	0xe1,0xf8,0x98,0x11,0x69,0xd9,0x8e,0x94,0x9b,0x1e,0x87,0xe9,0xce,0x55,0x28,0xdf,
+	0x8c,0xa1,0x89,0x0d,0xbf,0xe6,0x42,0x68,0x41,0x99,0x2d,0x0f,0xb0,0x54,0xbb,0x16
+};
+
+#define ADD_ROUND_KEY(x) \
+	state[ 1] ^= key[x+ 1]; state[ 2] ^= key[x+ 2];\
+	state[ 3] ^= key[x+ 3]; state[ 4] ^= key[x+ 4];\
+	state[ 5] ^= key[x+ 5]; state[ 6] ^= key[x+ 6];\
+	state[ 7] ^= key[x+ 7]; state[ 8] ^= key[x+ 8];\
+	state[ 9] ^= key[x+ 9]; state[10] ^= key[x+10];\
+	state[11] ^= key[x+11]; state[12] ^= key[x+12];\
+	state[13] ^= key[x+13]; state[14] ^= key[x+14];\
+	state[15] ^= key[x+15]; *state ^= key[x]; 
+
+#define SUB_BYTES() \
+	state[ 1] = SBOX[state[ 1]]; state[ 2] = SBOX[state[ 2]];\
+	state[ 3] = SBOX[state[ 3]]; state[ 4] = SBOX[state[ 4]];\
+	state[ 5] = SBOX[state[ 5]]; state[ 6] = SBOX[state[ 6]];\
+	state[ 7] = SBOX[state[ 7]]; state[ 8] = SBOX[state[ 8]];\
+	state[ 9] = SBOX[state[ 9]]; state[10] = SBOX[state[10]];\
+	state[11] = SBOX[state[11]]; state[12] = SBOX[state[12]];\
+	state[13] = SBOX[state[13]]; state[14] = SBOX[state[14]];\
+	state[15] = SBOX[state[15]]; *state = SBOX[*state];
+
+#define SHIFT_ROWS() \
+	a_0 = state[ 5]; b_0 = state[10]; c_0 = state[15];\
+	a_1 = state[ 9]; b_1 = state[14]; c_1 = state[ 3];\
+	a_2 = state[13]; b_2 = state[ 2]; c_2 = state[ 7];\
+	a_3 = state[ 1]; b_3 = state[ 6]; c_3 = state[11];\
+	state[ 1] = a_0; state[ 2] = b_0; state[ 3] = c_0;\
+	state[ 5] = a_1; state[ 6] = b_1; state[ 7] = c_1;\
+	state[ 9] = a_2; state[10] = b_2; state[11] = c_2;\
+	state[13] = a_3; state[14] = b_3; state[15] = c_3;
+
+#define MIX_COLLUMS() \
+	a_0 = *state; b_0 = (*state) << 1; a_1 = state[1]; b_1 = state[1] << 1;\
+	a_2 = state[2]; b_2 = state[2] << 1; a_3 = state[3]; b_3 = state[3] << 1;\
+	if ((*state) & 0x80) b_0 ^= 0x1b; if (state[1] & 0x80) b_1 ^= 0x1b; \
+	if (state[2] & 0x80) b_2 ^= 0x1b; if (state[3] & 0x80) b_3 ^= 0x1b; \
+	*state = b_0 ^ a_3 ^ a_2 ^ b_1 ^ a_1; state[1] = b_1 ^ a_0 ^ a_3 ^ b_2 ^ a_2;\
+	state[2] = b_2 ^ a_1 ^ a_0 ^ b_3 ^ a_3; state[3] = b_3 ^ a_2 ^ a_1 ^ b_0 ^ a_0;\
+	a_0 = state[4]; b_0 = state[4] << 1;\
+	a_1 = state[5]; b_1 = state[5] << 1;\
+	a_2 = state[6]; b_2 = state[6] << 1;\
+	a_3 = state[7]; b_3 = state[7] << 1;\
+	if (state[4] & 0x80) b_0 ^= 0x1b; if (state[5] & 0x80) b_1 ^= 0x1b; \
+	if (state[6] & 0x80) b_2 ^= 0x1b; if (state[7] & 0x80) b_3 ^= 0x1b; \
+	state[4] = b_0 ^ a_3 ^ a_2 ^ b_1 ^ a_1; state[5] = b_1 ^ a_0 ^ a_3 ^ b_2 ^ a_2;\
+	state[6] = b_2 ^ a_1 ^ a_0 ^ b_3 ^ a_3; state[7] = b_3 ^ a_2 ^ a_1 ^ b_0 ^ a_0;\
+	a_0 = state[8]; b_0 = state[8] << 1;\
+	a_1 = state[9]; b_1 = state[9] << 1;\
+	a_2 = state[10]; b_2 = state[10] << 1;\
+	a_3 = state[11]; b_3 = state[11] << 1;\
+	if (state[ 8] & 0x80) b_0 ^= 0x1b; if (state[ 9] & 0x80) b_1 ^= 0x1b; \
+	if (state[10] & 0x80) b_2 ^= 0x1b; if (state[11] & 0x80) b_3 ^= 0x1b; \
+	state[ 8] = b_0 ^ a_3 ^ a_2 ^ b_1 ^ a_1; state[ 9] = b_1 ^ a_0 ^ a_3 ^ b_2 ^ a_2;\
+	state[10] = b_2 ^ a_1 ^ a_0 ^ b_3 ^ a_3; state[11] = b_3 ^ a_2 ^ a_1 ^ b_0 ^ a_0;\
+	a_0 = state[12]; b_0 = state[12] << 1;\
+	a_1 = state[13]; b_1 = state[13] << 1;\
+	a_2 = state[14]; b_2 = state[14] << 1;\
+	a_3 = state[15]; b_3 = state[15] << 1;\
+	if (state[12] & 0x80) b_0 ^= 0x1b; if (state[13] & 0x80) b_1 ^= 0x1b; \
+	if (state[14] & 0x80) b_2 ^= 0x1b; if (state[15] & 0x80) b_3 ^= 0x1b; \
+	state[12] = b_0 ^ a_3 ^ a_2 ^ b_1 ^ a_1; state[13] = b_1 ^ a_0 ^ a_3 ^ b_2 ^ a_2;\
+	state[14] = b_2 ^ a_1 ^ a_0 ^ b_3 ^ a_3; state[15] = b_3 ^ a_2 ^ a_1 ^ b_0 ^ a_0;
+
+#endif // (!defined(__aarch64__) || !defined(__ARM_FEATURE_CRYPTO)) && (!defined(__x86_64__) || !defined(__AES__))
+
+static inline void aes(uint8_t* state, const uint8_t* key)
+{
+#if defined(__aarch64__) && defined(__ARM_FEATURE_CRYPTO)
+	__asm__ volatile(
+		"ld1  {v0.16b},[%0]    \n"
+		"ld1  {v1.16b,v2.16b,v3.16b},[%1] \n"
+		"aese v0.16b,v1.16b    \n"
+		"aesmc v0.16b,v0.16b    \n"
+		"aese v0.16b,v2.16b    \n"
+		"eor  v0.16b,v0.16b,v3.16b \n"
+		"st1  {v0.16b},[%0]    \n"
+		: /* only output is in *state */
+		: "r"(state), "r"(key)
+		: "v0", "v1", "v2", "v3", "cc", "memory");
+
+	// Use -maes to get this one
+#elif defined(__x86_64__) && defined(__AES__)
+	__asm__ volatile(
+		"movups (%0), %%xmm0   \n"
+		"movups (%1), %%xmm1   \n"
+		"pxor  %%xmm1,%%xmm0   \n"
+		"movups 16(%1),%%xmm2   \n"
+		"movups 32(%1),%%xmm1   \n"
+		"aesenc %%xmm2,%%xmm0   \n" 
+		"aesenclast %%xmm1,%%xmm0 \n"
+		"movups %%xmm0, (%0) \n"
+		: /* only output is in *state */
+		: "r"(state), "r" (key)
+		: "xmm0", "xmm1", "xmm2", "cc", "memory");
+#else
+	uint8_t a_0, a_1, a_2, a_3, b_0, b_1, b_2, b_3, c_0, c_1, c_2, c_3;
+	ADD_ROUND_KEY(0)
+	SUB_BYTES()
+	SHIFT_ROWS()
+	MIX_COLLUMS()
+	ADD_ROUND_KEY(16)
+	SUB_BYTES()
+	SHIFT_ROWS()
+	ADD_ROUND_KEY(32)
+#endif
+}
 
 uint32_t crc32c_table[256] = {
 	0x00000000, 0x77073096, 0xee0e612c, 0x990951ba,
@@ -78,223 +201,50 @@ uint32_t crc32c_table[256] = {
 	0xb40bbe37, 0xc30c8ea1, 0x5a05df1b, 0x2d02ef8d
 };
 
-uint32_t crc32(uint32_t msg) {
-	uint32_t crc = 0xFFFFFFFF;
 #if defined(__aarch64__) && defined(__ARM_FEATURE_CRC32)
-	__asm__("crc32w %w0,%w0,%w1\n":"+r"(crc):"r"(msg));
+#define crc32p(y,x) __asm__("crc32w %w0,%w0,%w1\n":"+r"(y):"r"(x));
 #else
-	crc=crc^msg;
-	crc=crc32c_table[crc&0xff]^(crc>>8);
-	crc=crc32c_table[crc&0xff]^(crc>>8);
-	crc=crc32c_table[crc&0xff]^(crc>>8);
-	crc=crc32c_table[crc&0xff]^(crc>>8);
+#define crc32p(y,x) \
+	y=crc32c_table[(x)&0xff]^((x)>>8);\
+	y=crc32c_table[(y)&0xff]^((y)>>8);\
+	y=crc32c_table[(y)&0xff]^((y)>>8);\
+	y=crc32c_table[(y)&0xff]^((y)>>8);
 #endif
-	return crc^0xFFFFFFFF;
-}
-
-uint32_t reverse(uint32_t x){
-#if defined(__aarch64__) && !defined(__x86_64__)
-	  __asm__("rbit %0":"+r"(v));
-#else
-	x = ((x & 0x55555555) << 1) | ((x & 0xAAAAAAAA) >> 1);
-	x = ((x & 0x33333333) << 2) | ((x & 0xCCCCCCCC) >> 2);
-	x = ((x & 0x0F0F0F0F) << 4) | ((x & 0xF0F0F0F0) >> 4);
-	x = ((x & 0x00FF00FF) << 8) | ((x & 0xFF00FF00) >> 8);
-	x = ((x & 0x0000FFFF) << 16) | ((x & 0xFFFF0000) >> 16);
-#endif
-	return x;
-}
-
-// reverse all bytes in the word _v_
-uint64_t swap(uint64_t v) {
-#if defined(__x86_64__)
-	__asm__("bswap %0":"+r"(v));
-#elif defined(__aarch64__)
-	__asm__("rev %0,%0\n":"+r"(v));
-#else
-	v=((v&0xff00ff00ff00ff00ULL)>>8)|((v&0x00ff00ff00ff00ffULL)<<8);
-	v=((v&0xffff0000ffff0000ULL)>>16)|((v&0x0000ffff0000ffffULL)<<16);
-	v=(v>>32)|(v<<32);
-#endif
-	return v;
-}
 
 
-void squash_0(uint8_t* data, uint8_t* out){
+void squash(uint8_t* data, uint8_t* out){
 	uint8_t   shift[4]   = {0};
-	uint64_t  key[2][2]  = {0};
-	uint64_t  crc_64[4]  = {0};
-	uint32_t* crc_32     = (uint32_t*)crc_64;
 	uint32_t* data_32    = (uint32_t*)data;
-	uint64_t* data_64    = (uint64_t*)data;
-	uint16_t* out_16     = (uint16_t*)out;
+	uint64_t* data0_64   = (uint64_t*)data;
+	uint64_t* data1_64   = (uint64_t*)&data[32];
+	uint32_t* out_32     = (uint32_t*)out;
 	uint64_t* out_64     = (uint64_t*)out;
-	crc_32[0] = crc32(data_32[0]);
-	crc_32[1] = crc32(data_32[1]);
-	crc_32[2] = crc32(data_32[2]);
-	crc_32[3] = crc32(data_32[3]);
-	crc_64[2] = (data_64[2] + crc_64[0]) ^ (data_64[2] / crc_64[0]);
-	crc_64[3] = (data_64[3] + crc_64[1]) ^ (data_64[3] / crc_64[1]);
-	out_64[0] = crc_64[0]; out_64[1] = crc_64[1];
-	out_64[2] = crc_64[2]; out_64[3] = crc_64[3];
-	shift[0] = out_16[15]&0x3f;
-	shift[2] = out_16[ 0]&0x3f;
-	shift[1] = 64-shift[0];
+	out_64[2] = data0_64[2]; out_64[3] = data0_64[3];
+	crc32p(*data_32, *out_32);
+	crc32p(data_32[1], out_32[1]);
+	crc32p(data_32[2], out_32[2]);
+	crc32p(data_32[3], out_32[3]);
+	out_64[2]+= data0_64[2]; out_64[3]+= data0_64[3];
+	*shift = out[31]&0x3f;
+	shift[2] = (*out)&0x3f;
+	shift[1] = 64-(*shift);
 	shift[3] = 64-shift[2];
-	key[1][0] = (out_64[0]>>shift[0]) | (out_64[0]<<(shift[1]));
-	key[1][1] = (out_64[1]>>shift[0]) | (out_64[1]<<(shift[1]));
-	key[0][0] = (out_64[2]<<shift[2]) | (out_64[2]>>(shift[3]));
-	key[0][1] = (out_64[3]<<shift[2]) | (out_64[3]>>(shift[3]));
-	aes(out     , (uint8_t*)key[0]);
-	aes(&out[16], (uint8_t*)key[1]);
+	*data1_64   = ((*out_64)>>(*shift)) | ((*out_64)<<(shift[1]));
+	data1_64[1] = (out_64[1]>>shift[0]) | (out_64[1]<<(shift[1]));
+	data1_64[2] = (out_64[2]<<shift[2]) | (out_64[2]>>(shift[3]));
+	data1_64[3] = (out_64[3]<<shift[2]) | (out_64[3]>>(shift[3]));
+	aes(out     ,  data);
+	aes(&out[16], &data[16]);
 	return;
 }
 
-// Squash_1 uses a lookup in addition to the previous operations
-// to force a hasher to have 256B read-only scratchpad in L1 cache
-void squash_1(uint8_t* data, uint8_t* scratchpad, uint8_t* out){
-	uint8_t   shift[4]   = {0};
-	uint64_t  key[2][2]  = {0};
-	uint64_t  divr[2]    = {0};
-	uint64_t  crc_64[4]  = {0};
-	uint32_t* crc_32     = (uint32_t*)crc_64;
-	uint8_t*  crc_8      = (uint8_t*)crc_64;
-	uint32_t* data_32    = (uint32_t*)data;
-	uint64_t* data_64    = (uint64_t*)data;
-	uint16_t* out_16     = (uint16_t*)out;
-	uint64_t* out_64     = (uint64_t*)out;
-	crc_32[0] = crc32(data_32[0]);
-	crc_32[1] = crc32(data_32[1]);
-	crc_32[2] = crc32(data_32[2]);
-	crc_32[3] = crc32(data_32[3]);
-	crc_32[4] = ((uint32_t*)&scratchpad[crc_8[ 0]])[0];
-	crc_32[5] = ((uint32_t*)&scratchpad[crc_8[ 4]])[0];
-	crc_32[6] = ((uint32_t*)&scratchpad[crc_8[ 8]])[0];
-	crc_32[7] = ((uint32_t*)&scratchpad[crc_8[12]])[0];
-	crc_32[0] = reverse(crc_32[0]);
-	crc_32[1] = reverse(crc_32[1]);
-	crc_32[6] = reverse(crc_32[6]);
-	crc_32[7] = reverse(crc_32[7]);
-	crc_64[1] = swap(crc_64[1]);
-	crc_64[2] = swap(crc_64[2]);
-	divr[0] = (data_64[2] + crc_64[2]) ^ (data_64[2] / crc_64[0]);
-	divr[1] = (data_64[3] + crc_64[3]) ^ (data_64[3] / crc_64[1]);
-	out_64[0] = crc_64[0]^divr[0]; out_64[1] = crc_64[1]^divr[0];
-	out_64[2] = crc_64[2]^divr[1]; out_64[3] = crc_64[3]^divr[1];
-	shift[0] = out_16[15]&0x3f;
-	shift[2] = out_16[ 0]&0x3f;
-	shift[1] = 64-shift[0];
-	shift[3] = 64-shift[2];
-	key[1][0] = (out_64[0]>>shift[0]) | (out_64[0]<<(shift[1]));
-	key[1][1] = (out_64[1]>>shift[0]) | (out_64[1]<<(shift[1]));
-	key[0][0] = (out_64[2]<<shift[2]) | (out_64[2]>>(shift[3]));
-	key[0][1] = (out_64[3]<<shift[2]) | (out_64[3]>>(shift[3]));
-	aes(out     , (uint8_t*)key[0]);
-	aes(&out[16], (uint8_t*)key[1]);
-	return;
-}
+int main(){
+	uint64_t data64[8] = {0};
+	uint8_t* data      = (uint8_t*)data64;
+	uint32_t ctr       = 1UL<<31;
+	do{
+		squash(data, data); 
+	}while(--ctr);
+	printf("Res: %016jx\n",*data64);
 
-// Difference from Squash_1 is a 16bit integer is used to obtain the
-// data from the scratchpad. (-> 64KiB scratchpad)
-void squash_2(uint8_t* data, uint8_t* scratchpad, uint8_t* out){
-	uint8_t   shift[4]   = {0};
-	uint64_t  key[2][2]  = {0};
-	uint64_t  divr[2]    = {0};
-	uint64_t  crc_64[4]  = {0};
-	uint32_t* crc_32     = (uint32_t*)crc_64;
-	uint16_t* crc_16     = (uint16_t*)crc_64;
-	uint32_t* data_32    = (uint32_t*)data;
-	uint64_t* data_64    = (uint64_t*)data;
-	uint16_t* out_16     = (uint16_t*)out;
-	uint64_t* out_64     = (uint64_t*)out;
-	crc_32[0] = crc32(data_32[0]);
-	crc_32[1] = crc32(data_32[1]);
-	crc_32[2] = crc32(data_32[2]);
-	crc_32[3] = crc32(data_32[3]);
-	crc_32[4] = ((uint32_t*)&scratchpad[crc_16[0]])[0];
-	crc_32[5] = ((uint32_t*)&scratchpad[crc_16[2]])[0];
-	crc_32[6] = ((uint32_t*)&scratchpad[crc_16[4]])[0];
-	crc_32[7] = ((uint32_t*)&scratchpad[crc_16[6]])[0];
-	crc_32[0] = reverse(crc_32[0]);
-	crc_32[1] = reverse(crc_32[1]);
-	crc_32[6] = reverse(crc_32[6]);
-	crc_32[7] = reverse(crc_32[7]);
-	crc_64[1] = swap(crc_64[1]);
-	crc_64[2] = swap(crc_64[2]);
-	divr[0] = (data_64[2] + crc_64[2]) ^ (data_64[2] / crc_64[0]);
-	divr[1] = (data_64[3] + crc_64[3]) ^ (data_64[3] / crc_64[1]);
-	out_64[0] = crc_64[0]^divr[0]; out_64[1] = crc_64[1]^divr[0];
-	out_64[2] = crc_64[2]^divr[1]; out_64[3] = crc_64[3]^divr[1];
-	shift[0] = out_16[15]&0x3f;
-	shift[2] = out_16[ 0]&0x3f;
-	shift[1] = 64-shift[0];
-	shift[3] = 64-shift[2];
-	key[1][0] = (out_64[0]>>shift[0]) | (out_64[0]<<(shift[1]));
-	key[1][1] = (out_64[1]>>shift[0]) | (out_64[1]<<(shift[1]));
-	key[0][0] = (out_64[2]<<shift[2]) | (out_64[2]>>(shift[3]));
-	key[0][1] = (out_64[3]<<shift[2]) | (out_64[3]>>(shift[3]));
-	aes(out     , (uint8_t*)key[0]);
-	aes(&out[16], (uint8_t*)key[1]);
-	return;
-}
-
-// Difference from Squash_2 is a 32bit integer is used to obtain the
-// data from the dataset. (-> 4 GiB dataset)
-void squash_3(uint8_t* data, uint64_t* dataset, uint8_t* out){
-	uint8_t   shift[4]      = {0};
-	uint64_t  key[2][2]     = {0};
-	uint64_t  divr[2]       = {0};
-	uint64_t  crc_64[4]     = {0};
-	uint32_t* crc_32        = (uint32_t*)crc_64;
-	uint16_t* crc_16        = (uint16_t*)crc_64;
-	uint32_t* data_32       = (uint32_t*)data;
-	uint64_t* data_64       = (uint64_t*)data;
-	uint16_t* out_16        = (uint16_t*)out;
-	uint64_t* out_64        = (uint64_t*)out;
-	uint16_t  temp_storage  = 0;
-	uint8_t   j             = 0;
-	crc_32[0] = crc32(data_32[0]);
-	crc_32[1] = crc32(data_32[1]);
-	crc_32[2] = crc32(data_32[2]);
-	crc_32[3] = crc32(data_32[3]);
-	crc_32[4] = ((uint32_t*)&dataset[crc_32[0]&0x1ffffffc])[0];
-	crc_32[5] = ((uint32_t*)&dataset[crc_32[1]&0x1ffffffc])[2];
-	crc_32[6] = ((uint32_t*)&dataset[crc_32[2]&0x1ffffffc])[4];
-	crc_32[7] = ((uint32_t*)&dataset[crc_32[3]&0x1ffffffc])[6];
-	for(uint16_t i=1;i<ACCESSES;i++){
-		j = i&7;
-		crc_32[4] = ((uint32_t*)&dataset[crc_32[4]&0x1ffffffc])[j];
-		crc_32[5] = ((uint32_t*)&dataset[crc_32[5]&0x1ffffffc])[j];
-		crc_32[6] = ((uint32_t*)&dataset[crc_32[6]&0x1ffffffc])[j];
-		crc_32[7] = ((uint32_t*)&dataset[crc_32[7]&0x1ffffffc])[j];
-		temp_storage = crc_16[10];
-		crc_16[10]   = crc_16[ 9];
-		crc_16[ 9]   = temp_storage;
-		temp_storage = crc_16[13];
-		crc_16[13]   = crc_16[14];
-		crc_16[14]   = temp_storage;
-	}
-	crc_32[0] = reverse(crc_32[0]);
-	crc_32[1] = reverse(crc_32[1]);
-	crc_32[6] = reverse(crc_32[6]);
-	crc_32[7] = reverse(crc_32[7]);
-	crc_64[1] = swap(crc_64[1]);
-	crc_64[2] = swap(crc_64[2]);
-	divr[0]  = (data_64[2] + crc_64[2]);
-	divr[1]  = (data_64[3] + crc_64[3]);
-	divr[0] ^= (data_64[2] / crc_64[0]);
-	divr[1] ^= (data_64[3] / crc_64[1]);
-	out_64[0] = crc_64[0]^divr[0]; out_64[1] = crc_64[1]^divr[0];
-	out_64[2] = crc_64[2]^divr[1]; out_64[3] = crc_64[3]^divr[1];
-	shift[0] = out_16[15]&0x3f;
-	shift[2] = out_16[ 0]&0x3f;
-	shift[1] = 64-shift[0];
-	shift[3] = 64-shift[2];
-	key[1][0] = (out_64[0]>>shift[0]) | (out_64[0]<<(shift[1]));
-	key[1][1] = (out_64[1]>>shift[0]) | (out_64[1]<<(shift[1]));
-	key[0][0] = (out_64[2]<<shift[2]) | (out_64[2]>>(shift[3]));
-	key[0][1] = (out_64[3]<<shift[2]) | (out_64[3]>>(shift[3]));
-	aes(out     , (uint8_t*)key[0]);
-	aes(&out[16], (uint8_t*)key[1]);
-	return;
 }
