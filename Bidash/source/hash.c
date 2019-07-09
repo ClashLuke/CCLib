317fb8689b8f6105953b0c7bb84ed45deed3172b
ClashLuke <luke@clashproject.com>
2019-07-09 10:31:59 +0200

    Increased entropy heavily, made algorithm a slight bit slower

--- Bidash/source/hash.c
+++ Bidash/source/hash.c
@@ -10,17 +10,17 @@
 
 
 void mash_full(uint8_t* data, uint8_t* dataset){
-	uint8_t   cache0[BLOCK+8] = {0};
+	uint8_t   cache0[BLOCK+16] = {0};
 	uint8_t   cache1[BLOCK+8] = {0};
 	uint64_t  item      = *(uint64_t*)&data[64];  
 	uint32_t* item32    = (uint32_t*)&data[64]; 
 	const uint64_t diff = *(uint64_t*)&data[72];
 	calcDataset(data, dataset);
 	for(uint64_t j=0; j<ITEMS; j+=BLOCK){
-		memcpy((void*)cache0, (void*)&dataset[j], BLOCK+8);
+		memcpy((void*)cache0, (void*)&dataset[j], BLOCK+16);
 		for(uint32_t a=0; a<BLOCK; a++){
-			item ^= *(uint64_t*)&cache0[a];
-			if(item < diff){
+			if((item ^ *(uint64_t*)&cache0[a]) < diff){
+				item ^= *(uint64_t*)&cache0[8+a];	
 				for(uint64_t k=j+a+1; k<ITEMS; k+=BLOCK){
 					memcpy(cache1, &dataset[k], BLOCK+8);
 					for(uint32_t b=0; b<BLOCK; b++){
@@ -30,9 +30,9 @@ void mash_full(uint8_t* data, uint8_t* dataset){
 							return;
 						}
 					}
-				}	
+				}
+				item ^= *(uint64_t*)&cache0[8+a];
 			}
-			item ^= *(uint64_t*)&cache0[a];
 		}
 	}
 }
